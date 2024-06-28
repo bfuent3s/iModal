5285f7a58b87179bf4a3ddb5498c7481909fdd72
Bernardo Fuentes <bfuentes@itivos.com>
2024-06-28 17:42:58 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,84 @@
+
 # iModal
- iModal es una librería de JavaScript que permite crear y gestionar modales de manera eficiente y moderna para aplicaciones web. Esta librería facilita la creación de múltiples modales que se superponen, gestionando automáticamente las llamadas AJAX y la navegación hacia atrás, tanto en dispositivos Android como iPhone, mediante la interpretación de botones físicos y gestos táctiles.
+
+iModal es una librería de JavaScript que permite crear y gestionar modales de manera eficiente y moderna para aplicaciones web. Esta librería facilita la creación de múltiples modales que se superponen, gestionando automáticamente las llamadas AJAX y la navegación hacia atrás, tanto en dispositivos Android como iPhone, mediante la interpretación de botones físicos y gestos táctiles.
+
+## Características
+
+- **Superposición de Modales:** Permite abrir múltiples modales uno sobre otro, similar al comportamiento de aplicaciones nativas.
+- **Gestión de Navegación:** Maneja el botón de atrás (o gestos) para cerrar modales, proporcionando una experiencia de usuario fluida.
+- **Compatibilidad con AJAX:** Facilita las llamadas AJAX dentro de los modales.
+- **Soporte para Gestos:** Compatible con gestos táctiles en dispositivos móviles para una navegación intuitiva.
+- **Interfaz Intuitiva:** Diseñada para integrarse fácilmente en cualquier aplicación web, mejorando la experiencia de usuario.
+
+## Instalación
+
+Puedes incluirlo directamente en tu proyecto con un script tag:
+
+\`\`\`html
+<link rel="stylesheet" href="path/to/imodal.min.css">
+<script src="path/to/imodal.min.js"></script>
+\`\`\`
+
+## Uso
+
+Aquí tienes un ejemplo básico de cómo usar iModal:
+
+\`\`\`javascript
+// Inicializar iModal
+const modal = new iModal();
+
+// Abrir un modal
+async function shoContent(param = null)
+{
+	let url_site = "https://itivos.com"; // your domain
+	let controller_ajax_uri = url_site + "my_controller/ajax"; // your controller
+	let action = controller_ajax_uri + "?resource=show_form" // Show a form, save , etc;
+	try {
+		const eventShow = new iModal();
+		eventShow.title = "Title to my modal";
+		eventShow.actionUri = action;
+		eventShow.dataToSend = {param: param}; // If your needd send parametters to controller
+		await eventShow.show();
+	} catch (error) {
+		console.error("General error: " + error);
+	}	
+} 
+\`\`\`
+
+## Opciones
+
+- \`title\`: Título del modal.
+- \`actionUri\`: Url de solicitud.
+- \`dataToSend\`: Array con los datos a enviar para procesar (Si es necesario)
+- \`show\`: Mostrar el modal
+
+## Compatibilidad
+
+iModal es compatible con los siguientes navegadores:
+
+- Chrome
+- Firefox
+- Safari
+- Edge
+- Opera
+
+## Contribuciones
+
+¡Las contribuciones son bienvenidas! Por favor, abre un issue o un pull request en el repositorio de GitHub.
+
+## Licencia
+
+Este proyecto está licenciado bajo la licencia MIT con una cláusula de atribución. Consulta el archivo \`LICENSE\` para más detalles.
+
+MIT License
+
+© [2024] [Bernardo Fuentes | Itivos Developers]
+
+Por la presente se concede permiso, sin cargo, a cualquier persona que obtenga una copia de este software y los archivos de documentación asociados (el "Software"), para tratar en el Software sin restricciones, incluyendo sin limitación los derechos de usar, copiar, modificar, fusionar, publicar, distribuir, sublicenciar y/o vender copias del Software, y para permitir a las personas a quienes se les proporcione el Software a hacerlo, sujeto a las siguientes condiciones:
+
+El aviso de copyright anterior y este aviso de permiso se incluirán en todas las copias o partes sustanciales del Software.
+
+EL SOFTWARE SE PROPORCIONA "TAL CUAL", SIN GARANTÍA DE NINGÚN TIPO, EXPRESA O IMPLÍCITA, INCLUYENDO PERO NO LIMITADO A LAS GARANTÍAS DE COMERCIABILIDAD, IDONEIDAD PARA UN PROPÓSITO PARTICULAR Y NO INFRACCIÓN. EN NINGÚN CASO LOS AUTORES O TITULARES DEL COPYRIGHT SERÁN RESPONSABLES POR NINGUNA RECLAMACIÓN, DAÑO O OTRA RESPONSABILIDAD, YA SEA EN UNA ACCIÓN DE CONTRATO, AGRAVIO O DE OTRO MODO, QUE SURJA DE, FUERA DE O EN CONEXIÓN CON EL SOFTWARE O EL USO U OTRO TIPO DE ACCIONES EN EL SOFTWARE.
+
+Se debe mantener la atribución al autor original en cualquier uso del Software. No se permite utilizar este software como parte de un proyecto cuyo objetivo principal sea competir directamente con el trabajo original sin obtener permiso previo del autor.
---
+++ src/imodal.js
@@ -0,0 +1,356 @@
+/*
+MIT License
+
+Copyright (c) 2024 Bernardo Fuentes @bfuent3s
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+const modalInstances = [];
+class iModal {
+  constructor() {
+    // Contenido HTML
+    this.html = 
+	  	`<div class="modal-header preview-modal">
+        <h3>
+          <i class="material-icons modal-close close-modal-instance">west</i>
+            <span class="title">
+            </span>
+        </h3>
+		  </div>
+	    <div style="display: block;">
+    		<div class="imodal-spinner"></div>
+        <!-- Contenido real aquí -->
+        <div>
+			</div>
+	  `;
+    // URL para ajax
+    this.actionUri = '';
+    // Parámetros para enviar por ajax
+    this.dataToSend = '' // Ejemplo: {isin: this.isin};
+    // Método predeterminado POST; GET
+    this.method = 'POST';
+    // Título
+    this.title = '';
+    // Identificador único para el modal
+    this.modalId = '';
+    // full o alert
+    this.type = 'full';
+    // true para habilitar pantalla completa; false para deshabilitar
+    this.transparent = false;
+    // true para habilitar el botón de más; false para deshabilitar
+    this.moreButton = false;
+
+    /** Solo para tipo de modal confirm */
+    // Título para alerta
+    this.alertTitle = '';
+    // Mensaje para el modal de confirmación
+    this.message = '';
+    // Información adicional para el modal
+    this.isin = '';
+    // Función de devolución de llamada para modal de confirmación
+    this.confirmFuntion = '';
+    /** FIN/// Solo para tipo de modal confirm */
+    this.transitionDirection = 'right-to-left';
+    this.showContentAfterLoad = false;
+  }
+
+   /**
+   * Muestra el modal.
+   * - Crea una instancia modal con las propiedades especificadas.
+   * - Establece el título si se proporciona.
+   * - Ajusta el z-index basado en las instancias existentes.
+   * - Añade la instancia modal al body.
+   * - Agrega un nuevo estado al historial del navegador.
+   */
+  async show() {
+    let $modalInstance;
+
+    if (this.transparent) {
+      $modalInstance = $('<div class="modal-instance modal-transparent ' + this.type + '"></div>');
+    } else {
+      $modalInstance = $('<div class="modal-instance ' + this.type + '"></div>');
+    }
+
+    $modalInstance.html(this.html);
+
+    var currentInstance = $(".modal-instance").length;
+    var result = 1000 + currentInstance;
+    $modalInstance.css('z-index', result);
+
+    if (this.title !== "") {
+      $modalInstance.find('.title').text(this.title);
+    }
+
+    if (this.moreButton) {
+      $modalInstance.find(".more_button").show();
+    }
+
+    $("body").append($modalInstance);
+    this.modalId = 'modal_' + Date.now();
+    $modalInstance.attr('modalId', this.modalId);
+    $modalInstance.attr('id', this.modalId);
+    history.pushState({ modalId: this.modalId }, null, null);
+
+    if (this.type === "full") {
+      $modalInstance.addClass(`slide-${this.transitionDirection}`);
+    }
+
+    modalInstances.push(this);
+
+    // Espera a que termine la animación de apertura antes de ejecutar fadeIn
+    $modalInstance.find('.modal-content').hide();
+    imodalDisableScroll();
+    try {
+      await new Promise(resolve => {
+        $modalInstance.on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+          resolve();
+        });
+      });
+
+      try {
+        const handleCatchError = async () => {
+          this.html = await ajaxRequest(this.actionUri, this.dataToSend, this.method);
+
+          /*
+          // Verificar si la respuesta es JSON
+          var jsonResponse;
+
+          try {
+            jsonResponse = JSON.parse(this.html);
+            if (jsonResponse.code === 'session_no_logged') {
+              var url_site = $("#url_site").val();
+              var lang = $("#language").val();
+              window.location.replace(url_site + lang + "/module/itivos_viaticos/home");
+              return;
+            }
+          } catch (parseError) {
+          }
+          */
+          $modalInstance.html(this.html);
+          $modalInstance.find('.modal-content').fadeIn('fast');
+
+          if (this.type === "alert") {
+            $modalInstance.fadeIn("slow");
+          }
+
+          const priceInputs = document.querySelectorAll('.price');
+          for (let i = 0; i < priceInputs.length; i++) {
+            const input = priceInputs[i];
+            if (!input.hasAttribute('data-autonumeric-initialized')) {
+              new AutoNumeric(input).northAmerican();
+              input.setAttribute('data-autonumeric-initialized', 'true');
+            }
+          }
+        };
+
+        // Llama a la función asincrónica
+        await handleCatchError();
+      } catch (error) {
+        console.error(error);
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  close() {
+    const $modalInstance = $(`#${this.modalId}`);
+	  if (this.type === "alert") {
+	    $modalInstance.hide('200', () => {
+	      $modalInstance.remove();
+	    });
+	  } else {
+	    // Agrega una clase para la animación de cierre
+	    $modalInstance.addClass(`slide-out-${this.transitionDirection}`);
+	    // Espera a que termine la animación antes de eliminar la instancia
+	    $modalInstance.on('animationend webkitAnimationEnd oAnimationEnd MSAnimationEnd', () => {
+	      $modalInstance.remove();
+	    });
+	  }
+  }
+}
+//End Modal instance
+
+class Confirm extends iModal {
+  constructor() {
+    super();
+    this.alertTitle = '';
+    this.message = '';
+    this.isin = '';
+    this.confirmFunction = '';
+  }
+
+  /**
+   * Establece las propiedades específicas para el modal de Confirmación.
+   * @param {string} alertTitle - Título para la alerta.
+   * @param {string} message - Mensaje para el modal de Confirmación.
+   * @param {string} isin - Información adicional para el modal.
+   * @param {Function} confirmFunction - Función de devolución de llamada para el modal de Confirmación.
+   */
+  setConfirmProperties(alertTitle, message, isin = null, confirmFunction = null) {
+    this.alertTitle = alertTitle;
+    this.message = message;
+    this.isin = isin;
+    this.type = "alert";
+    this.confirmFunction = confirmFunction;
+    this.transparent = true;
+    this.html = 
+    	`<div class="clearDivConfirm">
+    			<div class="confirm modal_confirm theme_white">
+		        <div class="modal-header">
+		            <span>${this.alertTitle}</span>
+		        </div>
+		        <div class="modal-content">
+		            <span>${this.message}</span>
+		        </div>
+		        <div class="modal-footer">
+		            <ul>
+		                <li>
+		                    <span onclick="${confirmFunction}" class="confirmed_dialog loading_full_screen_enable">Confirmar</span>
+		                </li>
+		                <li>
+		                    <a href="#!">Cancelar</a>
+		                </li>
+		            </ul>
+		    		</div> 
+		    	</div>
+    	 </div>
+    	`;
+  }
+
+  /**
+   * Muestra el modal de Confirmación.
+   * Anula el método show de la clase padre (iModal).
+   */
+  show() {
+    super.show(); // Llama al método show de la clase padre
+
+    // Personaliza las propiedades del modal de Confirmación
+    if (this.alertTitle !== "") {
+      $(`#${this.modalId} .title`).text(this.alertTitle);
+    }
+    $(`#${this.modalId} .modal-content`).show();
+    // Personalización adicional basada en las propiedades del modal de Confirmación
+    // (Puedes personalizar esta parte según tus necesidades)
+
+    // Ejemplo: Muestra el mensaje en el modal
+    if (this.message !== "") {
+      $(`#${this.modalId} .message`).text(this.message);
+    }
+    // Ejemplo: Adjunta un evento de clic al botón de Confirmar
+    $(`#${this.modalId} .confirmButton`).on('click', () => {
+      // Llama a la función de confirmación si se proporciona
+      if (typeof this.confirmFunction === 'function') {
+        this.confirmFunction();
+      }
+      // Cierra el modal
+      //$(`#${this.modalId}`).remove();
+    });
+  }
+}
+async function ajaxRequest(url, dataToSend = false, method = "POST") {
+	// Validar si dataToSend es un objeto FormData
+  const isFormData = dataToSend instanceof FormData;
+  const ajaxConfig = {
+    url: url,
+    type: method,
+    contentType: isFormData ? false : 'application/x-www-form-urlencoded',
+    processData: isFormData ? false : true,
+    enctype: isFormData ? 'multipart/form-data' : undefined,
+  };
+
+  // Agregar la propiedad 'data' solo si dataToSend está presente
+  if (dataToSend !== false) {
+    ajaxConfig.data = dataToSend;
+  }
+
+  try {
+    const response = await $.ajax(ajaxConfig);
+    // Parsear la respuesta JSON y manejar la sesión caducada
+    handleJsonResponse(response);
+    return response;
+  } catch (error) {
+    console.error("Error: " + error);
+    // Puedes decidir qué hacer en caso de error aquí (lanzar una excepción, manejar de otra manera, etc.)
+    throw error;
+  }
+}
+function handleJsonResponse(response) {
+  try {
+    var jsonResponse = JSON.parse(response);
+    if (jsonResponse.code === 'session_no_logged') {
+      var url_site = $("#url_site").val();
+      var lang = $("#language").val();
+      window.location.replace(url_site + lang + "/module/itivos_viaticos/app");
+    }
+  } catch (error) {
+    // Si la respuesta no es un JSON válido, simplemente continuar
+  }
+}
+function getLastModal()
+{
+	var lastInstance = $(".modal-instance:last");
+	return lastInstance;
+}
+async function handleBackButton() {
+  const lastModalInstance = modalInstances.pop();
+  if (lastModalInstance) {
+    lastModalInstance.close();
+  }
+  if (modalInstances.length === 0) {
+    imodalEnableScroll();
+  }
+}
+function getOppositeDirection(direction) {
+  switch (direction) {
+    case 'left-to-right':
+      return 'right-to-left';
+    case 'right-to-left':
+      return 'left-to-right';
+    case 'bottom-to-top':
+      return 'top-to-bottom';
+    case 'top-to-bottom':
+      return 'bottom-to-top';
+    default:
+      return 'left-to-right'; // Valor predeterminado en caso de dirección no válida
+  }
+}
+// Asignar la función al evento de retroceso (popstate)
+$(window).on('popstate', handleBackButton);
+// Boton indepenediente para  (popstate)
+$("body").on('click', '.close-modal-instance', async function(event) {
+  const $this = $(this);  // Almacena la referencia a $(this)
+  $this.addClass('close-modal-efect');
+
+  setTimeout(function() {
+    $this.removeClass('close-modal-efect');
+    handleBackButton();
+  }, 500);
+});
+var scrollPosition;
+
+function imodalDisableScroll() {
+    scrollPosition = window.pageYOffset;
+    document.body.classList.add('imodal-scroll-disabled');
+}
+
+function imodalEnableScroll() {
+    document.body.classList.remove('imodal-scroll-disabled');
+    window.scrollTo(0, scrollPosition);
+}
